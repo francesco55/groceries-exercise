03265a93655ebc8941b28cc8fb82d48b3b318f5f
Francesco Zeolla <fdz2@georgetown.edu>
2020-01-27 17:03:11 -0500

    finished everything

--- groceries.py
+++ groceries.py
@@ -25,7 +25,73 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+#print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+product_count = len(products)
+
+print("----------------------")
+print("THERE ARE " + str(product_count) + " PRODUCTS:")
+print("----------------------")
+
+def sort_by_name(p):
+    return p["name"]
+sorted_products = sorted(products, key=sort_by_name)
+
+for p in sorted_products:
+    price_usd = "${0:.2f}".format(p["price"])
+    print(" + " + p["name"] + " (" + str(price_usd) +")")
+
+departments = []
+for p in products:
+    departments.append(p["department"])
+    #if p["department"] not in departments:
+     #   departments.append(p["department"])
+
+unique_departments = list(set(departments))
+
+departments_count = len(unique_departments)
+
+print("------------------------")
+print("There are " + str(departments_count) + " Departments")
+print("------------------------")
+
+unique_departments.sort()
+
+for d in unique_departments:
+    matching_products = [p for p in products if p["department"] == d]
+    matching_product_count = len(matching_products)
+    if matching_product_count > 1:
+        label = "products"
+    else:
+        label = "product"
+    print(d.title() + " (" + str(matching_product_count) + " " + label + ")")
+
+#for p in products: 
+ #   print(f"{p["name"]} ({i["price"]})")
+#
+#
+#--------------
+#THERE ARE 20 PRODUCTS:
+#--------------
+# + All-Seasons Salt ($4.99)
+# + Chocolate Fudge Layer Cake ($18.50)
+# + Chocolate Sandwich Cookies ($3.50)
+# + Cut Russet Potatoes Steam N' Mash ($4.25)
+# + Dry Nose Oil ($21.99)
+# + Fresh Scent Dishwasher Cleaner ($4.99)
+# + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+# + Green Chile Anytime Sauce ($7.99)
+# + Light Strawberry Blueberry Yogurt ($6.50)
+# + Mint Chocolate Flavored Syrup ($4.50)
+# + Overnight Diapers Size 6 ($25.50)
+# + Peach Mango Juice ($1.99)
+# + Pizza For One Suprema Frozen Pizza ($12.50)
+# + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+# + Pure Coconut Water With Orange ($3.50)
+# + Rendered Duck Fat ($9.99)
+# + Robust Golden Unsweetened Oolong Tea ($2.49)
+# + Saline Nasal Mist ($16.00)
+# + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+# + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
